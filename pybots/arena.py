1b0178a3bd3bdb2dd15ed8a839f44df1d73bb57b
Bill Kuhns <wkuhns@vecs.org>
2022-10-02 13:26:32 -0500

    Clean compile with tkinter

--- pybots/arena.py
+++ pybots/arena.py
@@ -1,38 +1,200 @@
 import tkinter
 import time
- 
-class myCanvas(tkinter.Canvas):
-    
-  def makeBot(self, x, y, color):
-    return super(myCanvas, self).create_polygon(x-3,y-3, x-3,y+3, x+3,y+3, x+3,y-3, fill="Red", outline="Black", width=1)
-    #return super(myCanvas, self).create_polygon(50,50, 50,55, 55,55, 55,50, fill="red", outline="Black", width=1)
+import socket
+import sys
+import selectors
+import types
+import random
+import math
+import robot 
 
+
+def serviceConnection(key, mask):
+  sock = key.fileobj
+  data = key.data
+  if mask & selectors.EVENT_READ:
+    recv_data = sock.recv(1024)  # Should be ready to read
+    if recv_data:
+      data.outb = recv_data
+      #print(f"Command {data.outb!r} from {data.addr}")
+      cms = data.outb.decode("utf-8")
+      cmds = cms.split(";")
+      botIndex = int(cmds[0]) - 1
+      botCmd = cmds[1]
+      if botCmd == "scan":
+        #print("Scan commd: ", cms)
+        botParm1 = cmds[2]
+        botParm2 = cmds[3]
+        robots[botIndex].myScan(int(botParm1),int(botParm2))
+      if botCmd == "place":
+        botIndex = placeBot(sock)
+        recv_data = ""
+        data.outb = ""
+        return
+      if botCmd == "drive":
+        #print(f"Drive #{botIndex}: {cmds[2]} {cmds[3]}")
+        botParm1 = cmds[2]
+        botParm2 = cmds[3]
+        robots[botIndex].drive(int(botParm1),int(botParm2))
+        data.outb = ""
+        recv_data = ""
+        return
+      if botCmd == "fire":
+        botParm1 = cmds[2]
+        botParm2 = cmds[3]
+        robots[botIndex].myFire(int(botParm1),int(botParm2))
+        data.outb = ""
+        recv_data = ""
+        return
+      if botCmd == "setDirection":
+        botParm1 = cmds[2]
+        robots[botIndex].setSpeedGoal(int(botParm1))
+        #reply = "%d;%d" % (robots[botIndex].x, 4)
+        data.outb = ""
+        recv_data = ""
+        return
+    else:
+      # Client has closed socket, close our end too
+      print(f"Closing connection to {data.addr}")
+      sel.unregister(sock)
+      sock.close()
+    data.outb = ""
+
+def accept_wrapper(sock):
+  conn, addr = sock.accept()  # Should be ready to read
+  print(f"Accepted connection from {addr}")
+  conn.setblocking(False)
+  data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+  events = selectors.EVENT_READ | selectors.EVENT_WRITE
+  sel.register(conn, events, data=data)
+
+# Subclass of tkinter.Canvas with positive Y axis and arena scaling
+class myCanvas(tkinter.Canvas):
+
+  def __init__(self, window, height, width):
+    super().__init__(window, height=f'{height}', width=f'{width}')
+    self.height = height
+    self.width = width
+    self.xscale = width/1000
+    self.yscale = height/1000
+
+  def makeBot(self, x, y, color):
+    x = x * self.xscale
+    y = self.height - y * self.yscale
+    return super(myCanvas, self).create_polygon(x-3,y-3, x-3,y+3, x+3,y+3, x+3,y-3, fill=color, outline="Black", width=1)
+
+  # Move thing by x and y increments
   def move(self,thing,x,y):
+    x = x * self.xscale
+    y = y * self.yscale    
     super(myCanvas, self).move(thing,x,y*-1)
 
-def createFrame():
+# Window that frames the arena canvas
+def createFrame(width, height):
   Window = tkinter.Tk()
   Window.title("Arena")
-  Window.geometry('1050x1050')
+  Window.geometry(f'{width}x{height}')
   return Window
- 
 
-def createArena(Window):
-  #canvas = tkinter.Canvas(Window, height=1000, width=1000)
-  canvas = myCanvas(Window, height=1000, width=1000)
-  canvas.configure(bg="Blue")
+# Arena canvas 
+def createArena(window, height, width):
+  canvas = myCanvas(window, height, width)
+  canvas.configure(bg="Green")
   canvas.pack(expand=False)
   return canvas
 
-frame = createFrame()
-arena = createArena(frame)
-#bot = arena.create_polygon(50,50, 50,55, 55,55, 55,50, fill="red", outline="Black", width=1)
-bot = arena.makeBot(50, 50, "Blue")
+def placeBot(sock):
+  x = 0
+  for q in range (0, 3):
+    if quads[q].used == 0:
+      x = 1
+  
+  if x == 0:
+    print("Quadrant allocation error. Start over.")
+    return 0
 
-while True:
-  arena.move(bot,0,1)
-  frame.update()
-  time.sleep(.033)
 
+  # Pick a random quadrant
+  q = random.randint(0,3)
+  while quads[q].used == 1:
+    q = random.randint(0,3)
+  print (f"picked {q}")
+  quads[q].used = 1
+  r = len(robots)
+  print(f"placing {r+1}")
+  x = random.randint(0,300) + quads[q].x
+  y = random.randint(0,300) + quads[q].y
 
-#animate_ball(frame,arena, Ball_min_movement, Ball_min_movement)
+  robots.append(robot.serverRobot(r+1, arena))
+
+  robots[r].place(sock, x, y)
+  print(f"Bots: {robots[r].index}")
+
+  print (f"placed {r}")
+  return(robots[r].index)
+
+class quad:
+  def __init__(self):
+    self.used = 0
+    self.x = 0
+    self.y = 0
+
+quads = [quad(), quad(), quad(), quad()]
+quads[0].x = 100
+quads[0].y = 100
+quads[1].x = 100
+quads[1].y = 600
+quads[2].x = 600
+quads[2].y = 100
+quads[3].x = 600
+quads[3].y = 600
+
+# Create frame and arena - same size
+frame = createFrame(500,500)
+arena = createArena(frame, 500, 500)
+sel = selectors.DefaultSelector()
+robots = []
+
+def main():
+
+  #bot = arena.makeBot(750, 250, "lightgreen")
+
+  startTime = time.time()
+
+  HOST = "127.0.0.1"               # Symbolic name meaning all available interfaces
+  PORT = 50007              # Arbitrary non-privileged port
+
+  lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  lsock.bind((HOST, PORT))
+  lsock.listen()
+  print(f"Listening on {(HOST, PORT)}")
+  lsock.setblocking(False)
+  sel.register(lsock, selectors.EVENT_READ, data=None)
+
+  try:
+    while True:
+      # don't block
+      events = sel.select(timeout=-10)
+      for key, mask in events:
+        # listening socket
+        if key.data is None:
+          accept_wrapper(key.fileobj)
+        else:
+          serviceConnection(key, mask)
+      print("a")
+      #for r in bots():
+      #  r.update()
+      #for s in shells():
+      #  s.update()
+      #arena.move(bot,0,1)
+      frame.update()
+      time.sleep(.033)
+      print("b")
+  except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+  finally:
+    sel.close()
+    sys.exit()
+
+main()
+
--- pybots/robot.py
+++ pybots/robot.py
@@ -1,75 +1,327 @@
-import socket
-import asyncore
-import asynchat
+import random
+import math
+import time
 
-class EchoClient(asynchat.async_chat):
-    """Sends messages to the server and receives responses.
-    """
+# Code for robot instance on server
+class serverRobot():
+  lastReport = 0
+  braking = False
+  coasting = False
 
-    # Artificially reduce buffer sizes to illustrate
-    # sending and receiving partial messages.
-    ac_in_buffer_size = 64
-    ac_out_buffer_size = 64
+  def __init__(self, index, arena):
+    self.index = index
+    self.arena = arena
+    self.used = 0
+    self.name = "Robot"
+    self.quadrant = 0
+    self.deltax = 0
+    self.deltay = 0
+    self.sinfactor = 0   # sin(dir) in radians divided by tick rate
+    self.cosfactor = 0
+    self.status = "D"
+    self.color = 0
+    self.pingnotify = 0   # ID of bot who scanned me
+    self.proc = 0        # pointer to object in user#s program space
+                          # containing ping and die methods
+    self.dir = 45
+    self.dirgoal = 45
+    self.dirdelta = 0    # +/- increment to turn
+    self.speed = 1       # current speed in meters/sec
+    self.speedgoal = 0   # requested speed in meters/sec
+    self.speedx = 0
+    self.speedy = 0      # current axis movement in pixels per tick
+    self.shells = 4
+    self.mHeat = 0       # motor heat
+    self.bHeat = 0       # barrel heat
+    self.reload = 0      # ticks till ready
+    self.fire = 0        # ticks till shell hits
+    self.newshot = 0     # flag for new shell. Cleared by display
+    self.tx = 0          # target x,y
+    self.ty = 0
+    self.dx = 0          # shell x, y
+    self.dy = 0
+    self.scan = 0       # scanning flag - at present,
+                          # set by scan method and cleared
+                          # when display accesses scan data
+    self.sdir = 45
+    self.sres = 10
+    self.lastscanned = 0  # ID of last 'bot who I scanned
+    self.health = 100
+    self.maxSpeed = 20
+
+  def sendMessage(self,reply):
+    #print("sending: ", reply)
+    reply = reply + ':'
+    self.sock.send(reply.encode("utf-8"))
+    #time.sleep(.001)
+
+  def myScan(self, dir, res):
+    self.sdir = dir
+    self.sres = res
+
+    theta1 = (self.sdir - self.sres/2)
+    if theta1 < 0:
+      theta1 += 360
+    theta2 = (self.sdir + self.sres/2)
+    endx = (1500 * math.cos(theta1 / 57.3)) + self.x
+    endy = (1500 * math.sin(theta1 / 57.3)) + self.y  
+    pygame.draw.line(self.arena, (0, 0, 0), (self.x,1000-self.y), (endx,1000-endy))
+
+    endx = (1500 * math.cos(theta2 / 57.3)) + self.x
+    endy = (1500 * math.sin(theta2 / 57.3)) + self.y  
+    pygame.draw.line(self.arena, (0, 0, 0), (self.x,1000-self.y), (endx,1000-endy))
+
+    # See if we saw anyone
+    dist = 0
+    for r in robots:
+      if r.status == "A" and r.index != self.index:
+        rtheta = math.atan2((r.y - self.y), (r.x - self.x))
+        if rtheta < 0:
+          rtheta += (2 * math.pi)
+        rtheta = rtheta * 57.3
+
+        if rtheta > theta1 and rtheta < theta2:
+          dist = math.sqrt((r.y - self.y)**2 + (r.x - self.x)**2)
+          self.ping(r.index)
+
+    # Add in errors: error based on scan width
+    if self.bHeat > 35:
+      dist = 0
+
+    if dist > 0:
+      dist += random.randint(-1 * self.sres, self.sres)
+      print (dist)
+      # Barrel heat
+      if random.randint(0,1):
+        dist += self.bHeat
+      else:
+        dist -= self.bHeat
+
+    #print("Dist, bheat", dist, self.bHeat)
+
+    reply = "%d;scan;%d;%d" % (self.index, dist, dir)
+    self.sendMessage(reply)
+  
+  def myFire(self, dir, range):
+    if range >= 40 and range <= 700 and dir >= 0 and dir <= 359: 
+      shells[self.index-1].fire(self.x, self.y, dir, range)
+      self.bHeat += 20;
+      reply = "%d;fire;0" % (self.index)
+    else:
+      reply = "%d;fire;-1" % (self.index)
+    self.sendMessage(reply)
+
+  def report(self):
+    speed = int(self.speed*100/self.maxSpeed)
+    dir = (360 - self.dir) % 360
+    reply = "%d;status;%d;%d;%d;%d;%d;%d;%d" % (self.index,self.x,self.y,self.health,self.mHeat,speed,dir,self.pingnotify)
+    self.sendMessage(reply)
+
+  def ping(self, enemy):
+    reply = "%d;ping;%d" % (enemy, self.index)
+    robots[enemy-1].sock.send(reply.encode("utf-8"))
+
+  def place(self,sock, x, y):
     
-    def __init__(self, host, port, message):
-        self.message = message.encode('utf-8')
-        print(self.message)
-        self.received_data = []
-        #self.logger = logging.getLogger('EchoClient')
-        asynchat.async_chat.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        print('connecting to %s', (host, port))
-        self.connect((host, port))
-        return
-        
-    def handle_connect(self):
-        print('handle_connect()')
-        # Send the command
-        self.push(b'%d\n' % len(self.message))
-        # Send the data
-        self.push_with_producer(EchoProducer(self.message, buffer_size=self.ac_out_buffer_size))
-        # We expect the data to come back as-is, 
-        # so set a length-based terminator
-        self.set_terminator(len(self.message))
+    bot = self.arena.makeBot(750, 250, "lightgreen")
+    #self.x = self.rect.centerx
+    #self.y = 999 - self.rect.centery
+    self.sock = sock
+    self.name = "Temp"
+    self.used = 1
+    #self.quadrant = q
+    self.x = x
+    self.y = y
+    self.speed = 0
+    self.speedgoal = 0
+    self.dir = random.randint(0,359)
+    self.sinfactor = math.sin(self.dir / 57.3)
+    self.cosfactor = math.cos(self.dir / 57.3)
+    self.deltax = 0
+    self.deltay = 0
+    self.dirgoal = self.dir
+    self.dirdelta = 0
+    self.health = 100
+    self.shells = 4
+    self.status = "A"
+    self.mHeat = 0
+    self.bHeat = 0
+    self.reload = 0
+    self.fire = 0
+    self.scan = 0
+    #blitRotateCenter(arena, self.image, self.x, angle):
+    self.lastReport = time.time()
+    print (f"Placed x {self.x} y {self.y}")
+    reply = "0;place;%d;%d;%d;%d" % (self.index,self.x,self.y,self.dir)
+    print(reply)
+    self.sendMessage(reply)
+    return self.index
+
+  # set direction and speed
+  def drive(self, direction, speed):
+    #print (f"Processing {direction} {speed}")
+    speed = max(speed,0)
+    speed = min(speed,100)
+    # commanded speed in m/sec
+    speed = speed / 100 * self.maxSpeed
+
+    if (self.speed == 0):
+      self.coasting = False
+
+    # Are we coasting? do nothing until stopped
+    if self.coasting:
+      return
+
+    # Are we braking? Don't change speed goal
+    if (self.braking and self.speed <= self.speedgoal):
+      self.braking = False
+
+    if (not self.braking):
+      self.speedgoal = speed
+
+    self.dirgoal = (360-direction) % 360
+    delta = self.dirgoal - self.dir
+
+    if delta > 180:
+      delta = delta - 360
+    if delta <= -180:
+      delta = delta + 360
+
+    # Is commanded speed too fast? Coast to stop in current dir.
+    if abs(delta) > 75 and speed > 4 and self.speed > 4:
+        self.coasting = True
+    if abs(delta) > 50 and speed > 7.5 and self.speed > 7.5:
+        self.coasting = True
+    if abs(delta) > 25 and speed > 10 and self.speed > 10:
+        self.coasting = True
+
+    # is current speed too fast?
+    if abs(delta) > 75 and self.speed > 4:
+        self.braking = True
+        self.speedgoal = 4
+    if abs(delta) > 50 and self.speed > 7.5:
+        self.braking = True
+        self.speedgoal = 7.5
+    if abs(delta) > 25 and self.speed > 10:
+        self.braking = True
+        self.speedgoal = 10
+
+    if self.coasting:
+      delta = 0
+      self.speedgoal = 0
+      self.dirgoal = self.dir
+
+    self.dirdelta = delta
+    #print (f"Speed goal = {self.speedgoal}, dirgoal = {self.dirgoal}")
+
+  def update(self):
+    if self.used == 0:
+      return
+    #print(f"Processing {self.index}")
+    # adjust heading if we're turning
+    if self.dirdelta != 0:
+      # calculate turning rate
+      newrate = int(self.speed / 25)
+      if newrate == 4: 
+          rate = 30 / QUANTA
+      elif newrate == 3: 
+          rate = 30 / QUANTA
+      elif newrate == 2: 
+          rate = 40 / QUANTA
+      elif newrate == 1: 
+          rate = 60 / QUANTA
+      elif newrate == 0: 
+          rate = 90 / QUANTA
+      
+      delta = self.dirdelta
+      if (delta > rate):
+        delta = rate
+      if (delta < rate * -1):
+        delta = rate * -1
+      self.dir = (self.dir + delta) % 360
+      #print("dir: ", self.dir)
+      self.dirdelta = self.dirdelta - delta
+      # Cos(dir) in radians
+      # Precalculated to avoid doing it every frame
+      self.cosfactor = math.cos(self.dir / 57.3)
+      self.sinfactor = math.sin(self.dir / 57.3)
+      self.deltax = self.speed * self.cosfactor / QUANTA
+      self.deltay = self.speed * self.sinfactor / QUANTA * -1
+
+    # barrel cooling
+    self.bHeat = self.bHeat - (2 / QUANTA)
+    if self.bHeat < 0:
+      self.bHeat = 0
     
-    def collect_incoming_data(self, data):
-        """Read an incoming message from the client and put it into our outgoing queue."""
-        print('collect_incoming_data() -> (%d)\n"""%s"""', len(data), data)
-        self.received_data.append(data)
+    # motor heating
+    heat = (self.speed - 35) / (5 * QUANTA)
+    # accelerate cooling
+    if heat <= 0:
+      heat = heat * (30 / QUANTA) - 2
+    self.mHeat = self.mHeat + heat
+    if self.mHeat >= 200:
+        self.mHeat = 200
+        self.speed = 35
+        self.speedgoal = 35
+        self.deltax = self.speed * self.cosfactor / QUANTA
+        self.deltay = self.speed * self.sinfactor / QUANTA * -1
+    
+    if self.mHeat < 0:
+      self.mHeat = 0
 
-    def found_terminator(self):
-        print('found_terminator()')
-        received_message = ''.join(self.received_data)
-        if received_message == self.message:
-            print('RECEIVED COPY OF MESSAGE')
-        else:
-            print('ERROR IN TRANSMISSION')
-            print('EXPECTED "%s"', self.message)
-            print('RECEIVED "%s"', received_message)
-        return
+    # adjust speed
+    delta = self.speedgoal - self.speed
+    if delta != 0:
+      if (delta > (10 / QUANTA)):
+        delta = (10 / QUANTA)
+      if (delta < (-10 / QUANTA)):
+        delta = (-10 / QUANTA)
+      self.speed = self.speed + delta
+      # calculate per tick movement
+      self.deltax = self.speed * self.cosfactor / QUANTA
+      self.deltay = self.speed * self.sinfactor / QUANTA * -1
+      #print("Speed: ", self.speed, "Direction: ", self.dir, "Coasting: ", self.coasting, "Braking: ", self.braking)
 
-class EchoProducer(asynchat.simple_producer):
 
-    #logger = logging.getLogger('EchoProducer')
+    # Move the puppy
+    newx = self.x + self.deltax
+    if newx > 999 or newx < 0:
+      self.speed = 0
+      self.speedgoal = 0
+      self.deltax = 0
+      self.deltay = 0
+      #Call wound(i, 5)
+    else:
+      self.x = newx
+    
+    # y is upside down - 0 at top
+    newy = self.y + self.deltay
+    if newy > 999 or newy < 0:
+      self.speed = 0
+      self.speedgoal = 0
+      self.deltax = 0
+      self.deltay = 0
+      #Call wound(i, 5)
+    else:
+        self.y = newy
 
-    def more(self):
-        response = asynchat.simple_producer.more(self)
-        #self.logger.debug('more() -> (%s bytes)\n"""%s"""', len(response), response)
-        return response
+    #self.rect.move_ip(int(self.x-self.rect.centerx), int(1000-self.y-self.rect.centery))
 
-client = EchoClient('', 50007, message="message_data")
-asyncore.loop()
+    if self.reload > 0:
+      self.reload = self.reload - 1 / QUANTA
 
-#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sock.connect(('localhost', 50007))
-#print ("Connected to server")
-#data = """A few lines of data
-#to test the operation
-#of both server and client."""
-#for line in data.splitlines(  ):
-#    sock.sendall(line.encode('utf-8'))
-#    #sock.sendall('123;456')
-#    print ("Sent:", line)
-#    response = sock.recv(8192)
-#    print ("Received:", response)
-#sock.close(  )
+    if((time.time() - self.lastReport) > .5):
+      #print ("Setting...")
+      self.lastReport = time.time()
+      #print("Reporting...")
+      self.report()
+      #print("scanning")
+      #self.myScan()
+
+  def wound(self, damage):
+    self.health -= damage
+    print (f"Robot {self.index-1} health: {self.health}")
+    if self.health <= 0:
+      print ("Robot dead: ", self.index-1)
+      self.status = "D"
+
